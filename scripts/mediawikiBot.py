5852ddcd0351f538ade5139000e4dcb151d9745f
BASEFlow1 <67925673+BASEFlow1@users.noreply.github.com>
2022-09-05 19:07:15 +0200

    added a progress bar + fixed doLater

--- scripts/mediawikiBot.py
+++ scripts/mediawikiBot.py
@@ -2,8 +2,11 @@ import requests
 from json import load
 from os.path import exists
 import time
+from alive_progress import alive_bar
 
 URL = "https://disco-freelancer.fandom.com/api.php"
+delay = 1
+
 
 if exists("botPassword.json") and exists("wikitext.json"):
     with open("botPassword.json", "r") as f:
@@ -27,7 +30,7 @@ if exists("botPassword.json") and exists("wikitext.json"):
 
     request = session.get(url=URL, params=loginToken_params)
     data = request.json()
-    print(data)
+    #print(data)
     loginToken = data['query']['tokens']['logintoken']
 
     login_params = {
@@ -47,38 +50,53 @@ if exists("botPassword.json") and exists("wikitext.json"):
 
     request = session.get(url=URL, params=csrf_params)
     data = request.json()
-    print(data)
+    #print(data)
     csrfToken = data['query']['tokens']['csrftoken']
 
     doLater = []
-    for name, text in wikitext.items():
-        edit_params = {
-            "action": "edit",
-            "title": name,
-            "text": text,
-            "bot": True,
-            "format": "json",
-            "token": csrfToken
-        }
-        request = session.post(URL, data = edit_params)
-        data = request.json()
-        print(data)
-        try:
-            data['error']
-            doLater.append([name, text])
-        except:
-            pass
-        time.sleep(1)
-    for name, text in doLater:
-        edit_params = {
-            "action": "edit",
-            "title": name,
-            "text": text,
-            "bot": True,
-            "format": "json",
-            "token": csrfToken
-        }
-        request = session.post(URL, data = edit_params)
-        data = request.json()
-        print(data)
-        time.sleep(1)
+    with alive_bar(len(wikitext.keys()), dual_line=True, title="mediawikiBot.py") as bar:
+        for name, text in wikitext.items():
+            bar.text = f'-> Uploading: {name}'
+            edit_params = {
+                "action": "edit",
+                "title": name,
+                "text": text,
+                "bot": True,
+                "format": "json",
+                "token": csrfToken
+            }
+            request = session.post(URL, data = edit_params)
+            data = request.json()
+            #print(data)
+            try:
+                error = data['error']["code"]
+                doLater.append([name, text])
+                print(f"Error uploading {name}: {error}, trying again later...")
+            except:
+                pass
+            time.sleep(delay)
+            bar()
+    if doLater != []:
+        print("Retrying failed uploads...")
+        with alive_bar(len(doLater), dual_line=True, title="mediawikiBot.py") as bar:
+            for name, text in doLater:
+                bar.text = f'-> Uploading: {name}: {error}'
+                edit_params = {
+                    "action": "edit",
+                    "title": name,
+                    "text": text,
+                    "bot": True,
+                    "format": "json",
+                    "token": csrfToken
+                }
+                request = session.post(URL, data = edit_params)
+                data = request.json()
+                #print(data)
+                try:
+                    data['error']
+                    doLater.append([name, text])
+                    print(f"Error uploading {name}.")
+                except:
+                    pass
+                time.sleep(delay * 2.5)
+                bar()
