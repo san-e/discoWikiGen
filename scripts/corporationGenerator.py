194cb5a135a2f174cf3f7150d250ab63fc512ecf
BASEFlow1 <67925673+BASEFlow1@users.noreply.github.com>
2022-10-30 17:29:57 +0100

    poggers

---
+++ scripts/corporationGenerator.py
@@ -0,0 +1,19 @@
+import json
+import flint as fl
+from pprint import pprint
+
+with open("cconfig.json", "r") as f:
+    path = json.load(f)["freelancerPath"]
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+fl.set_install_path(path)
+
+results = []
+for faction in fl.factions:
+    if faction.nickname not in faction.name() and not faction.name().isspace():
+        if 'y' in input(f"Is this faction a corporation or guild?: {faction.name()}\ny/n"):
+            results.append(faction.name())
+
+pprint(results)
+config["pageGen"]["corporations"] = results
