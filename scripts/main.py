87f0bb0b3a74efa20c6af3f8df71ff61100a7c8f
BASEFlow1 <67925673+BASEFlow1@users.noreply.github.com>
2022-11-19 21:39:50 +0100

    we do a little refactoring

--- scripts/main.py
+++ scripts/main.py
@@ -34,68 +34,75 @@ def firstTimeSetup():
     botP3 = input("Bot-password: ")
     botCredentials = [f"{botP1}@{botP2}", botP3]
 
-    config["firstTimeSetupRan"] = True
-    with open("./config.json", "w") as f:
-        json.dump(config, f, indent=1)
     with open("./cConfig.json", "w") as f:
         json.dump({
             "freelancerPath": freelancerPath,
             "botCredentials": botCredentials
         }, f, indent=1)
 
-
-with open("./config.json", "r") as f:
-    config =  json.load(f)
-
-
-if not config["firstTimeSetupRan"]:
-    print("Running first time setup...")
-    firstTimeSetup()
+def nukeWiki():
+    print("Nuke the wiki before updating? y/N")
+    nuke = True if 'y' in input() else False
     clearConsole()
-
-print("Nuke the wiki before updating? y/N")
-nuke = True if 'y' in input() else False
-clearConsole()
+    return nuke
 
 
-print(
-"""Which of the following (if any) pages do you wish to update?
- (1) Systems
- (2) Ships
- (3) Bases
- (4) Factions
- (5) Commodities
- (6) Redirects
- (7) Special
- (8) Images
- (x) All of the above
-""")
-print("")
-selection = input()
-options = {
-    "systems": True if "1" in selection or "x" in selection else False,
-    "ships": True if "2" in selection or "x" in selection else False,
-    "bases": True if "3" in selection or "x" in selection else False,
-    "factions": True if "4" in selection or "x" in selection else False,
-    "commodities": True if "5" in selection or "x" in selection else False,
-    "redirects": True if "6" in selection or "x" in selection else False,
-    "special": True if "7" in selection or "x" in selection else False,
-    "images": True if "8" in selection or "x" in selection else False,
-    "nuke": nuke
-    }
-choices = [option for option, chosen in options.items() if chosen == True]
-clearConsole()
+def pagesToUpdate():
+    nuke = nukeWiki()
+    print(
+    """Which of the following (if any) pages do you wish to update?
+    (1) Systems
+    (2) Ships
+    (3) Bases
+    (4) Factions
+    (5) Commodities
+    (6) Redirects
+    (7) Special
+    (8) Images
+    (x) All of the above
+    """)
+    print("")
+    selection = input()
+    options = {
+        "systems": True if "1" in selection or "x" in selection else False,
+        "ships": True if "2" in selection or "x" in selection else False,
+        "bases": True if "3" in selection or "x" in selection else False,
+        "factions": True if "4" in selection or "x" in selection else False,
+        "commodities": True if "5" in selection or "x" in selection else False,
+        "redirects": True if "6" in selection or "x" in selection else False,
+        "special": True if "7" in selection or "x" in selection else False,
+        "images": True if "8" in selection or "x" in selection else False,
+        "nuke": nuke
+        }
+    clearConsole()
+    return [option for option, chosen in options.items() if chosen == True]
 
-print(
-f"""You chose the following:
+
+def callBot():
+    choices = pagesToUpdate()
+    print(
+    f"""You chose the following:
 {choices}
 Confirm? y/N
-""")
-if 'y' in input():
-    clearConsole()
-    flData = flWikiGen.main()
-    wikitext = pageGen.main(flData)
-    clearConsole()
-    mediawikiBot.main(wikidata = wikitext, choices=choices)
-else:
-    quit()
+    """)
+    if 'y' in input():
+        clearConsole()
+        flData = flWikiGen.main()
+        wikitext = pageGen.main(flData)
+        clearConsole()
+        mediawikiBot.main(wikidata = wikitext, choices=choices)
+    else:
+        quit()
+
+
+if __name__ == "__main__":
+    with open("./config.json", "r") as f:
+        config =  json.load(f)
+
+
+    if not os.path.exists("./cConfig.json"):
+        print("Running first time setup...")
+        firstTimeSetup()
+        clearConsole()
+
+    callBot()
