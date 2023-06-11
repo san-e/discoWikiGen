efc36f45d7ef64a428f4a176c85f4affc1cffc09
san-e <itzepyc@gmail.com>
2023-06-11 14:25:31 +0200

    shorten functions

--- scripts/mediawikiBot.py
+++ scripts/mediawikiBot.py
@@ -4,23 +4,6 @@ from os.path import exists, isdir, split
 from os import scandir
 import time
 from alive_progress import alive_bar
-import argparse
-
-argparser = argparse.ArgumentParser()
-argparser.add_argument(
-    "-n", "--nuke", help="Nuke all generated pages on the wiki", action="store_true"
-)
-argparser.add_argument("--systems", help="Update system pages", action="store_true")
-argparser.add_argument("--ships", help="Update ship pages", action="store_true")
-argparser.add_argument("-b", "--bases", help="Update base pages", action="store_true")
-argparser.add_argument("-f", "--factions", help="Update faction pages", action="store_true")
-argparser.add_argument("-i", "--images", help="Upload images", action="store_true")
-argparser.add_argument("-c", "--commodities", help="Update commodities", action="store_true")
-argparser.add_argument("-w", "--weapons", help="Update weapons", action="store_true")
-argparser.add_argument("--special", help="Update Special Pages", action="store_true")
-argparser.add_argument("-a", "--all", help="Update all Pages + Upload Images", action="store_true")
-argparser.add_argument("-r", "--redirects", help="Update Redirects", action="store_true")
-args = argparser.parse_args()
 
 with open("config.json", "r") as f:
     config = load(f)
@@ -30,40 +13,46 @@ delay = config["bot"]["delay"]
 
 
 def login(botPasswordPath):
-    if exists(botPasswordPath):
-        with open(botPasswordPath, "r") as f:
-            data = load(f)
-            botName, botPassword = data["botCredentials"][0], data["botCredentials"][1]
+    if not exists(botPasswordPath):
+        raise FileNotFoundError
 
-        session = requests.Session()
+    with open(botPasswordPath, "r") as f:
+        data = load(f)
+        botName, botPassword = data["botCredentials"][0], data["botCredentials"][1]
 
-        loginToken_params = {
-            "action": "query",
-            "format": "json",
-            "meta": "tokens",
-            "type": "login",
-        }
+    session = requests.Session()
 
-        request = session.get(url=URL, params=loginToken_params)
-        data = request.json()
-        loginToken = data["query"]["tokens"]["logintoken"]
+    loginToken_params = {
+        "action": "query",
+        "format": "json",
+        "meta": "tokens",
+        "type": "login",
+    }
 
-        login_params = {
-            "action": "login",
-            "lgname": botName,
-            "lgpassword": botPassword,
-            "lgtoken": loginToken,
-            "format": "json",
-        }
-        request = session.post(URL, data=login_params)
+    request = session.get(url=URL, params=loginToken_params)
+    data = request.json()
+    loginToken = data["query"]["tokens"]["logintoken"]
 
-        csrf_params = {"action": "query", "meta": "tokens", "format": "json"}
+    login_params = {
+        "action": "login",
+        "lgname": botName,
+        "lgpassword": botPassword,
+        "lgtoken": loginToken,
+        "format": "json",
+    }
+    request = session.post(URL, data=login_params)
 
-        request = session.get(url=URL, params=csrf_params)
-        data = request.json()
-        csrfToken = data["query"]["tokens"]["csrftoken"]
+    csrf_params = {
+        "action": "query",
+        "meta": "tokens",
+        "format": "json"
+    }
 
-        return session, csrfToken
+    request = session.get(url=URL, params=csrf_params)
+    data = request.json()
+    csrfToken = data["query"]["tokens"]["csrftoken"]
+
+    return session, csrfToken
 
 
 def regenerateTokens():
@@ -72,178 +61,144 @@ def regenerateTokens():
     session, csrfToken = login(config["bot"]["botPassword"])
 
 def uploadText(session, csrfToken, wikitext, titleText):
-    doLater = []
-    doLater2 = []
-    with alive_bar(len(wikitext.keys()), dual_line=True, title=titleText) as bar:
-        for name, text in wikitext.items():
-            bar.text = f"-> Updating: {name}"
-            edit_params = {
-                "action": "edit",
-                "title": name,
-                "text": text,
-                "bot": True,
-                "format": "json",
-                "token": csrfToken,
-            }
-            request = session.post(URL, data=edit_params)
-            data = request.json()
-            try:
-                error = data["error"]["code"]
-                doLater.append([name, text])
-                print(f"Error updating {name}: {error}, trying again later...")
-                if error == "badtoken":
-                    regenerateTokens()
-            except:
-                pass
-            time.sleep(delay)
-            bar()
-    while True:
-        if doLater != []:
-            print("Retrying failed edits...")
-            with alive_bar(len(doLater), dual_line=True, title=titleText) as bar:
-                for name, text in doLater:
-                    bar.text = f"-> Updating: {name}"
-                    edit_params = {
-                        "action": "edit",
-                        "title": name,
-                        "text": text,
-                        "bot": True,
-                        "format": "json",
-                        "token": csrfToken,
-                    }
-                    request = session.post(URL, data=edit_params)
-                    data = request.json()
-                    try:
-                        error = data["error"]["code"]
-                        doLater2.append([name, text])
-                        print(f"Error updating {name}: {error}, trying again later...")
-                        if error == "badtoken":
-                            regenerateTokens()
-                    except:
-                        doLater.remove([name, text])
-                    time.sleep(delay * 2)
+    def upload(toUpload):
+        failedUploads = {}
+        with alive_bar(len(toUpload.keys()), dual_line=True, title=titleText) as bar:
+            for name, text in toUpload.items():
+                bar.text = f"-> Updating: {name}"
+                edit_params = {
+                    "action": "edit",
+                    "title": name,
+                    "text": text,
+                    "bot": True,
+                    "format": "json",
+                    "token": csrfToken,
+                }
+                request = session.post(URL, data=edit_params)
+                data = request.json()
+                try:
+                    error = data["error"]["code"]
+                    failedUploads[name] = text
+                    print(f"Error updating {name}: {error}, trying again later...")
+                    if error == "badtoken":
+                        regenerateTokens()
+                except:
+                    pass
                     bar()
-        else:
-            break
-        if doLater2 != []:
-            doLater = doLater2
-        doLater2 = []
+                time.sleep(delay)
+        
+        return failedUploads
+
+    failures = upload(wikitext)
+
+    while failures:
+        failures = upload(failures)
 
 
 def uploadImages(session, csrfToken, titleImage, path="../dumpedData/images"):
-    subdirectories = []
+    subdirectories = set()
     if exists(path):
         with scandir(path) as dirs:
             for entry in dirs:
                 if isdir(entry.path):
-                    subdirectories.append(entry.path)
+                    subdirectories.add(entry.path)
 
-    allimages_params = {
-        "action": "query",
-        "format": "json",
-        "list": "allimages",
-        "aiprop": "comment",
-        "ailimit": 5000,
-    }
-    allimages = session.post(URL, data=allimages_params)
-    allimages = allimages.json()["query"]["allimages"]
-    allimages = {dic["name"].lower(): dic["comment"] for dic in allimages}
 
-    doLater = []
-    doLater2 = []
-    for directory in subdirectories:
-        with alive_bar(
-            len([x for x in scandir(directory)]), dual_line=True, title=titleImage
-        ) as bar:
-            for entry in scandir(directory):
-                if entry.name.endswith("png"):
-                    try:
-                        if allimages[entry.name] != config["bot"]["comment"]:
-                            print(
-                                f"Skipping {entry.name}, probably shouldn't be replaced."
-                            )
-                            bar()
-                            continue
-                    except:
-                        pass
-                    bar.text = (
-                        f"-> Uploading: {entry.name} from folder {split(directory)[-1]}"
-                    )
-                    upload_params = {
-                        "action": "upload",
-                        "filename": entry.name,
-                        "comment": config["bot"]["comment"],
-                        "format": "json",
-                        "token": csrfToken,
-                        "bot": True,
-                        "ignorewarnings": 1,
-                    }
-                    with open(entry.path, "rb") as fileParam:
-                        file = {"file": (entry.name, fileParam, "multipart/form-data")}
-                        request = session.post(URL, files=file, data=upload_params)
-                    data = request.json()
+    def getWikiImages():
+        allimages_params = {
+            "action": "query",
+            "format": "json",
+            "list": "allimages",
+            "aiprop": "comment",
+            "ailimit": 5000,
+        }
+        allimages = session.post(URL, data=allimages_params)
+        allimages = allimages.json()["query"]["allimages"]
+        allimages = {dic["name"].lower(): dic["comment"] for dic in allimages}
+        return allimages
 
-                    try:
-                        error = data["error"]["code"]
-                        if not error == "fileexists-no-change":
-                            print(
-                                f"Error uploading {entry.name}: {error}, trying again later..."
-                            )
-                            doLater.append(entry)
-                        else:
-                            print(
-                                f"File {entry.name} already exists on the wiki. {error}"
-                            )
-                    except:
-                        pass
-                    # time.sleep(delay)
-                    bar()
+    def upload(entries):
+        failedUploads = []
+        with alive_bar(len(entries), dual_line=True, title=titleImage) as bar:
+            for entry in entries:
+                if not entry["name"].endswith("png"):
+                    continue
 
-    while True:
-        if doLater != []:
-            print("Retrying failed uploads...")
-            with alive_bar(len(doLater), dual_line=True, title=titleImage) as bar:
-                for entry in doLater:
-                    if entry.name.endswith("png"):
-                        bar.text = f"-> Uploading: {entry.name}"
-                        upload_params = {
-                            "action": "upload",
-                            "filename": entry.name,
-                            "comment": config["bot"]["comment"],
-                            "format": "json",
-                            "bot": True,
-                            "token": csrfToken,
-                            "ignorewarnings": 1,
-                        }
-                        with open(entry.path, "rb") as fileParam:
-                            file = {
-                                "file": (entry.name, fileParam, "multipart/form-data")
-                            }
-                            request = session.post(URL, files=file, data=upload_params)
-                        data = request.json()
-                        try:
-                            error = data["error"]["code"]
-                            if not error == "fileexists-no-change":
-                                print(
-                                    f"Error uploading {entry.name}: {error}, trying again later..."
-                                )
-                                doLater2.append(entry)
-                            else:
-                                print(
-                                    f"File {entry.name} already exists on the wiki. {error}"
-                                )
-                        except:
-                            pass
-                        # time.sleep(delay * 2.5)
+                try:
+                    if allimages[entry['name']] != config["bot"]["comment"]:
+                        print(
+                            f"Skipping {entry['name']}, probably shouldn't be replaced."
+                        )
                         bar()
-        else:
-            break
-        if doLater2 != []:
-            doLater = doLater2
-        doLater2 = []
+                        continue
+                except:
+                    pass
+
+                bar.text = (
+                    f"-> Uploading: {entry['name']} from folder {split(directory)[-1]}"
+                )
+                upload_params = {
+                    "action": "upload",
+                    "filename": entry['name'],
+                    "comment": config["bot"]["comment"],
+                    "format": "json",
+                    "token": csrfToken,
+                    "bot": True,
+                    "ignorewarnings": 1,
+                }
+                with open(entry['path'], "rb") as fileParam:
+                    file = {"file": (entry['name'], fileParam, "multipart/form-data")}
+                    request = session.post(URL, files=file, data=upload_params)
+                data = request.json()
+
+                try:
+                    error = data["error"]["code"]
+                    if error == "fileexists-no-change":
+                        print(
+                            f"File {entry['name']} already exists on the wiki. {error}"
+                        )
+
+                    else:
+                        print(
+                            f"Error uploading {entry['name']}: {error}, trying again later..."
+                        )
+                        failedUploads.append({"name": entry['name'], "path": entry['path']})
+                except:
+                    pass
+                # time.sleep(delay)
+                bar()
+        return failedUploads
+    
+    allimages = getWikiImages()
+
+    failures = []
+    for directory in subdirectories:
+        entries = [{"name": entry.name, "path": entry.path} for entry in scandir(directory)]
+        failures = failures + upload(entries)
+
+    while failures:
+        failures = upload(failures)
 
 
 def nukeTheWiki(session, csrfToken, titleNuke):
+    def nuke():
+        with alive_bar(len(idsToNuke), dual_line=True, title=titleNuke) as bar:
+            for id in idsToNuke:
+                bar.text = f"Nuking pageID {id}"
+                nuke_params = {
+                    "action": "delete",
+                    "format": "json",
+                    "reason": "This page has been nuked!",
+                    "pageid": id,
+                    "token": csrfToken,
+                }
+
+                request = session.post(URL, nuke_params)
+                # print(request.json())
+
+                # time.sleep(delay) # seemingly not neccessary
+                bar()
+                
     queryNuke_params = {
         "action": "query",
         "generator": "categorymembers",
@@ -256,24 +211,10 @@ def nukeTheWiki(session, csrfToken, titleNuke):
     }
 
     request = session.post(URL, data=queryNuke_params)
-    idsToNuke = list(request.json()["query"]["pages"].keys())
+    idsToNuke = set(request.json()["query"]["pages"].keys())
 
-    with alive_bar(len(idsToNuke), dual_line=True, title=titleNuke) as bar:
-        for id in idsToNuke:
-            bar.text = f"Nuking pageID {id}"
-            nuke_params = {
-                "action": "delete",
-                "format": "json",
-                "reason": "This page has been nuked!",
-                "pageid": id,
-                "token": csrfToken,
-            }
+    nuke()
 
-            request = session.post(URL, nuke_params)
-            # print(request.json())
-
-            # time.sleep(delay) seemingly not neccessary
-            bar()
 
 
 def main(wikidata = None, choices = None):      
@@ -284,34 +225,34 @@ def main(wikidata = None, choices = None):
             wikidata = load(f)
 
     wikitext = {}
-    if args.systems or args.all or "systems" in choices:
+    if "systems" in choices:
         wikitext = wikitext | wikidata["Systems"]
-    if args.ships or args.all or "ships" in choices:
+    if "ships" in choices:
         wikitext = wikitext | wikidata["Ships"]
-    if args.bases or args.all or "bases" in choices:
+    if "bases" in choices:
         wikitext = wikitext | wikidata["Bases"]
-    if args.factions or args.all or "factions" in choices:
+    if "factions" in choices:
         wikitext = wikitext | wikidata["Factions"]
-    if args.commodities or args.all or "commodities" in choices:
+    if "commodities" in choices:
         wikitext = wikitext | wikidata["Commodities"]
-    if args.weapons or args.all or "weapons" in choices:
-        wikitext = wikitext | wikidata["Weapons"]#
-    if args.weapons or args.all or "cms" in choices:
+    if "weapons" in choices:
+        wikitext = wikitext | wikidata["Weapons"]
+    if "cms" in choices:
         wikitext = wikitext | wikidata["CMs"]
-    if args.weapons or args.all or "armor" in choices:
+    if "armor" in choices:
         wikitext = wikitext | wikidata["Armor"]
-    if args.weapons or args.all or "cloaks" in choices:
+    if "cloaks" in choices:
         wikitext = wikitext | wikidata["Cloaks"]
-    if args.weapons or args.all or "engines" in choices:
+    if "engines" in choices:
         wikitext = wikitext | wikidata["Engines"]
-    if args.weapons or args.all or "shields" in choices:
+    if "shields" in choices:
         wikitext = wikitext | wikidata["Shields"]
-    if args.redirects or args.all or "redirects" in choices:
+    if "redirects" in choices:
         wikitext = wikitext | wikidata["Redirects"]
-    if args.special or args.all or "special" in choices:
+    if "special" in choices:
         wikitext = wikitext | wikidata["Special"]
 
-    if args.nuke or "nuke" in choices:
+    if "nuke" in choices:
         nukeTheWiki(
             session=loginData[0],
             csrfToken=loginData[1],
@@ -324,7 +265,7 @@ def main(wikidata = None, choices = None):
         wikitext=wikitext,
         titleText=config["bot"]["titleText"],
     )
-    if args.images or args.all or "images" in choices:
+    if "images" in choices:
         uploadImages(
             session=loginData[0],
             csrfToken=loginData[1],
