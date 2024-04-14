077a16cfffd6431a517d03a6d0e76448904a936f
san-e <itzepyc@gmail.com>
2024-04-14 19:46:49 +0200

    download config files from the server + formatting

--- scripts/main.py
+++ scripts/main.py
@@ -4,12 +4,15 @@ import mediawikiBot
 import json
 import os
 from flint.paths import is_probably_freelancer
+import requests
+from bs4 import BeautifulSoup
+
 
 def clearConsole():
-    if os.name == 'nt':
-        os.system('cls')
+    if os.name == "nt":
+        os.system("cls")
     else:
-        os.system('clear')
+        os.system("clear")
 
 
 def firstTimeSetup():
@@ -21,31 +24,41 @@ def firstTimeSetup():
             break
         else:
             print("Path is not a valid Freelancer installation, trying again...")
-    
-    print("""Input the API-Link to your Wiki (default: "https://disco-freelancer.fandom.com/api.php"):""")
+
+    print(
+        """Input the API-Link to your Wiki (default: "https://disco-freelancer.fandom.com/api.php"):"""
+    )
     wikiLink = input("")
-    wikiLink = "https://disco-freelancer.fandom.com/api.php" if not wikiLink else wikiLink
+    wikiLink = (
+        "https://disco-freelancer.fandom.com/api.php" if not wikiLink else wikiLink
+    )
 
     clearConsole()
-    print(f"""Input your Bot Username and Password
+    print(
+        f"""Input your Bot Username and Password
     If you don't have one, navigate to /wiki/Special:BotPasswords to create one.
-    More info: https://www.mediawiki.org/wiki/Manual:Bot_passwords""")
+    More info: https://www.mediawiki.org/wiki/Manual:Bot_passwords"""
+    )
     botP1 = input("Wiki-Account-Username: ")
     botP2 = input("Bot-Name: ")
     botP3 = input("Bot-Password: ")
     botCredentials = [f"{botP1}@{botP2}", botP3]
 
     with open("./secret.json", "w") as f:
-        json.dump({
-            "freelancerPath": freelancerPath,
-            "URL": wikiLink,
-            "botCredentials": botCredentials
-        }, f, indent=1)
+        json.dump(
+            {
+                "freelancerPath": freelancerPath,
+                "URL": wikiLink,
+                "botCredentials": botCredentials,
+            },
+            f,
+            indent=1,
+        )
 
 
 def nukeWiki():
     print("Nuke the wiki before updating? y/N")
-    nuke = True if 'y' in input() else False
+    nuke = True if "y" in input() else False
     clearConsole()
     return nuke
 
@@ -53,7 +66,7 @@ def nukeWiki():
 def pagesToUpdate():
     nuke = nukeWiki()
     print(
-    """Which of the following (if any) pages do you wish to update?
+        """Which of the following (if any) pages do you wish to update?
     (a) Systems
     (b) Ships
     (c) Bases
@@ -69,26 +82,27 @@ def pagesToUpdate():
     (2) Special
     (3) Images
     (x) All of the above
-    """)
+    """
+    )
     print("")
     selection = input()
     options = {
-        "systems":      True if "a" in selection or "x" in selection else False,
-        "ships":        True if "b" in selection or "x" in selection else False,
-        "bases":        True if "c" in selection or "x" in selection else False,
-        "factions":     True if "d" in selection or "x" in selection else False,
-        "commodities":  True if "e" in selection or "x" in selection else False,
-        "weapons":      True if "f" in selection or "x" in selection else False,
-        "cms":          True if "g" in selection or "x" in selection else False,
-        "armor":        True if "h" in selection or "x" in selection else False,
-        "cloaks":       True if "i" in selection or "x" in selection else False,
-        "engines":      True if "j" in selection or "x" in selection else False,
-        "shields":      True if "k" in selection or "x" in selection else False,
-        "redirects":    True if "1" in selection or "x" in selection else False,
-        "special":      True if "2" in selection or "x" in selection else False,
-        "images":       True if "3" in selection or "x" in selection else False,
-        "nuke": nuke
-        }
+        "systems": True if "a" in selection or "x" in selection else False,
+        "ships": True if "b" in selection or "x" in selection else False,
+        "bases": True if "c" in selection or "x" in selection else False,
+        "factions": True if "d" in selection or "x" in selection else False,
+        "commodities": True if "e" in selection or "x" in selection else False,
+        "weapons": True if "f" in selection or "x" in selection else False,
+        "cms": True if "g" in selection or "x" in selection else False,
+        "armor": True if "h" in selection or "x" in selection else False,
+        "cloaks": True if "i" in selection or "x" in selection else False,
+        "engines": True if "j" in selection or "x" in selection else False,
+        "shields": True if "k" in selection or "x" in selection else False,
+        "redirects": True if "1" in selection or "x" in selection else False,
+        "special": True if "2" in selection or "x" in selection else False,
+        "images": True if "3" in selection or "x" in selection else False,
+        "nuke": nuke,
+    }
     clearConsole()
     return [option for option, chosen in options.items() if chosen == True]
 
@@ -96,29 +110,44 @@ def pagesToUpdate():
 def callBot():
     choices = pagesToUpdate()
     print(
-    f"""You chose the following:
+        f"""You chose the following:
 {choices}
 Confirm? y/N
-    """)
-    if 'y' in input():
+    """
+    )
+    if "y" in input():
         clearConsole()
         print("Dumping game data\n===================")
         flData = flWikiGen.main()
         wikitext = pageGen.main(flData)
         clearConsole()
-        mediawikiBot.main(wikidata = wikitext, choices=choices)
+        mediawikiBot.main(wikidata=wikitext, choices=choices)
     else:
         quit()
 
 
+def downloadServerConfig(url="https://discoverygc.com/gameconfigpublic/"):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = soup.find_all("a")
+    urls = {
+        "https://discoverygc.com/gameconfigpublic/" + link.get("href") for link in links
+    }
+
+    for url in urls:
+        r = requests.get(url)
+        if r.ok:
+            with open(f"./server_config/{url.split('/')[-1]}", "wb") as f:
+                f.write(r.content)
+
+
 if __name__ == "__main__":
     with open("./config.json", "r") as f:
-        config =  json.load(f)
-
+        config = json.load(f)
 
     if not os.path.exists("./secret.json"):
         print("Running first time setup...")
         firstTimeSetup()
         clearConsole()
-
+    downloadServerConfig()
     callBot()
