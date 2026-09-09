1352c61b67fc73361c5f4bf868ad77930c65d3df
san-e <itzepyc@proton.me>
2026-09-10 01:59:37 +0200

    test

--- scripts/dump.py
+++ scripts/dump.py
@@ -65,7 +65,6 @@ def render(renderer: str, out_path: str) -> None:
             secret["freelancer"],
             out_path,
         ),
-        stdout=subprocess.DEVNULL,
     )
     p.wait()
 
@@ -73,7 +72,6 @@ def render(renderer: str, out_path: str) -> None:
     for image in os.listdir(out_path):
         if not image.endswith(".png"):
             continue
-        print(f"Cropping: {image}              ", end="\r", flush=True)
         im = Image.open(f"{out_path}/{image}")
         im = im.crop(im.getbbox())
         im.save(f"{out_path}/{image}")
